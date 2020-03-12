177fa9c63544beffce516cb023b366a153804f68
Sciencmine <2631197967@qq.com>
2020-03-12 11:02:59 +0800

    Add index page

---
+++ app.js
@@ -0,0 +1,36 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        urlInput: "",
+        source: "",
+        md: "",
+        compiled: "",
+        showSource: false,
+        shouldSanitize: true,
+        statusCode: 200
+    },
+    methods: {
+        loadMd: function () {
+            superagent.get(app.urlInput).end(function (err, res) {
+                app.statusCode = res.statusCode, app.source = res.text;
+                app.loadSource();
+                app.compile();
+            });
+        },
+        compile: function () {
+            var markedProcessed = marked(app.source);
+            if (app.shouldSanitize) {
+                app.compiled = DOMPurify.sanitize(markedProcessed);
+            } else {
+                app.compiled = markedProcessed;
+            }
+        },
+        loadSource: function () {
+            if (app.showSource) {
+                app.md = marked("```markdown\n" + app.source + "\n```");
+            } else {
+                app.md = "";
+            }
+        }
+    }
+});
